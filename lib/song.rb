fe0db1a3a359e1cbd58d226b56c092d605da6cd6
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-15 17:32:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
-  include Paramble::InstanceMethods
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
